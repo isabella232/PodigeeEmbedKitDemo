8ca47945d7ff821249c1ded771165ed80db51917
Stefan Trauth <mail@stefantrauth.de>
2018-11-21 10:17:10 +0100

    create embed demo project player

--- PodigeeEmbedKitDemo/ViewController.swift
+++ PodigeeEmbedKitDemo/ViewController.swift
@@ -8,14 +8,57 @@
 
 import UIKit
 import PodigeeEmbedKit
+import AVKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var coverartImageView: UIImageView!
+    @IBOutlet weak var podcastTitleLabel: UILabel!
+    @IBOutlet weak var episodeTitle: UILabel!
+    @IBOutlet weak var playbackButton: UIButton!
+    
+    private var podcastEmbed: PodcastEmbed? {
+        didSet {
+            updateUI()
+        }
+    }
+    private var player: AVPlayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        PodigeeEmbedKit.embedDataForPodcastWith(domain: "podcast-news.podigee.io") { (podcastEmbed, error) in
+            if let error = error {
+                print(error.localizedDescription)
+            }
+            self.podcastEmbed = podcastEmbed
+        }
+    }
+    
+    private func updateUI() {
+        updateCoverart()
+        DispatchQueue.main.async {
+            self.podcastTitleLabel.text = self.podcastEmbed?.podcast.title
+            self.episodeTitle.text = self.podcastEmbed?.episode?.title
+        }
+    }
+    
+    private func updateCoverart() {
+        guard let coverartUrl = self.podcastEmbed?.episode?.coverUrl else { return }
+        URLSession.shared.dataTask(with: coverartUrl, completionHandler: { (data, response, error) in
+            guard let data = data else { return }
+            let image = UIImage(data: data)
+            DispatchQueue.main.async {
+                self.coverartImageView.image = image
+            }
+        }).resume()
     }
 
-
+    @IBAction func togglePlayback(_ sender: UIButton) {
+        guard let audioUrl = podcastEmbed?.episode?.media.mp3 else { return }
+        player = AVPlayer(url: audioUrl)
+        player?.play()
+    }
+    
 }
 
