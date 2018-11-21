c13b2f6d3c609aa7e9aa48cd2c52c0f6e156b8dd
Stefan Trauth <mail@stefantrauth.de>
2018-11-21 09:49:21 +0100

    Initial Commit

---
+++ PodigeeEmbedKitDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PodigeeEmbedKitDemo
+//
+//  Created by Stefan Trauth on 21.11.18.
+//  Copyright © 2018 Podigee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
